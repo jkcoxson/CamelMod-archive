d67467ae36bd1fdf1bc4c8dcbbf1859cb957a67e
jkcoxson <jkcoxson@gmail.com>
2021-02-21 15:50:50 -0700

    init

---
+++ src/main/java/com/jkcoxson/camelmod/Camelmod.java
@@ -0,0 +1,10 @@
+package com.jkcoxson.camelmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Camelmod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
